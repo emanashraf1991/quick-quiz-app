29cbc4fc43886e8109ccbfa9e188ff25adb38c08
Eman Ashraf <emanashraf1991@gmail.com>
2019-05-03 02:32:29 -0700

    FInalize the project

--- game.js
+++ game.js
@@ -15,20 +15,38 @@ let availableQuestions=[];
 
 let questions = [];
    
-fetch("questions.json")
-.then( res => {
-    return res.json();
+fetch("https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple")
+.then(res => {
+  return res.json();
 })
 .then(loadedQuestions => {
-    console.log(loadedQuestions);
-    questions= loadedQuestions;
-    startGame();
+  console.log(loadedQuestions.results);
+  questions = loadedQuestions.results.map(loadedQuestion => {
+    const formattedQuestion = {
+      question: loadedQuestion.question
+    };
+
+    const answerChoices = [...loadedQuestion.incorrect_answers];
+    formattedQuestion.answer = Math.floor(Math.random() * 3) + 1;
+    answerChoices.splice(
+      formattedQuestion.answer - 1,
+      0,
+      loadedQuestion.correct_answer
+    );
+
+    answerChoices.forEach((choice, index) => {
+      formattedQuestion["choice" + (index + 1)] = choice;
+    });
+
+    return formattedQuestion;
+  });
+  startGame();
 })
 .catch(err =>{
   console.error(err);
 })
   const CORRECT_BONUS=10;
-  const MAX_QUESTIONS=3;
+  const MAX_QUESTIONS=5;
 
   startGame= () =>{
       questionCounter=0;
@@ -36,6 +54,8 @@ fetch("questions.json")
       availableQuestions=[...questions];
       console.log(availableQuestions);
       getNewQuestion();
+      game.classList.remove("hidden");
+      loader.classList.add("hidden");
   };
 
   getNewQuestion = () =>{
@@ -50,11 +70,11 @@ if(availableQuestions.length===0 || questionCounter >= MAX_QUESTIONS){
 
     const questionIndex= Math.floor(Math.random() * availableQuestions.length);
     currentQuestion=availableQuestions[questionIndex];  
-    question.innerText=currentQuestion.question;
+    question.innerHTML=currentQuestion.question;
 
     choices.forEach(c => {
         const number=c.dataset['number'];
-        c.innerText=currentQuestion['choice'+number];
+        c.innerHTML=currentQuestion['choice'+number];
     })
 
     availableQuestions.splice(questionIndex,1);
